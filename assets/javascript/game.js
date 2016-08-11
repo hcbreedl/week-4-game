220df2aa5d6e43d5277da2b0c3409d31671e08e5
Houston Breedlove <wehaveaproblem16@Houstons-MacBook-Pro.local>
2016-08-11 13:32:54 -0400

    working game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,27 @@
 $( document ).ready(function() {
-
+	var newGame = function () {
 	var warrior = {
 		"hp": 100,
-		"ap": 40,
-		"ca": 30
+		"ap": 30,
+		"ca": 20
 	}
 
 	var girl = {
 		"hp": 100,
-		"ap": 30,
-		"ca": 40
+		"ap": 20,
+		"ca": 10
 	}
 
 	var goblin = {
 		"hp": 100,
-		"ap": 20,
-		"ca": 50
+		"ap": 10,
+		"ca": 40
 	}
 
 	var giant = {
 		"hp": 100,
-		"ap": 50,
-		"ca": 20
+		"ap": 40,
+		"ca": 10
 	}
 
 	var usersCharacter;
@@ -31,6 +31,7 @@ $( document ).ready(function() {
 	var opponent1;
 	var opponent2;
 	var opponent3;
+	var opponentsLeft = 3;
 
 	var defender;
 	var defenderCharacterStats;
@@ -40,10 +41,24 @@ $( document ).ready(function() {
 	var opponentsRow;
 	var defenderRow;
 	var attack;	
+
+	var resetUserCharacter = function() {
+		usersCharacter.remove();
+		usersCharacterStats;
+		displayUserStats.remove();
+	};
+	var resetDefender = function() {
+		defender.remove();
+		displayDefenderStats.remove();
+		defenderRow.remove();
+		defender;
+		defenderCharacterStats;
+	}
 	
 //============================
 // Selecting user's character:
 //============================
+	// var newGame = function () {
 	$(".character").on('click', function() {
 
 		// Selecting Warrior as usersCharacter
@@ -191,6 +206,7 @@ $( document ).ready(function() {
 			displayDefenderStats = $('<div id="displayDefenderStats" class="col-xs-2">').html('Health: ' + defenderCharacterStats.hp + '<br>Attack: ' + defenderCharacterStats.ap + '<br>Counter: ' + defenderCharacterStats.ca);
 			defenderRow.append(displayDefenderStats);
 		};
+	
 
 //===========================
 // Pressing the Attack Button
@@ -205,12 +221,112 @@ $( document ).ready(function() {
 			$('#displayUserStats').html('Health: ' + usersCharacterStats.hp + '<br>Attack: ' + usersCharacterStats.ap + '<br>Counter: ' + usersCharacterStats.ca);
 			$('#displayDefenderStats').html('Health: ' + defenderCharacterStats.hp + '<br>Attack: ' + defenderCharacterStats.ap + '<br>Counter: ' + defenderCharacterStats.ca);
 
-			console.log(usersCharacterStats);
-			console.log(defenderCharacterStats);
+			//===================
+			// Losing and Winning
+			//===================
+			if (usersCharacterStats.hp < 1) {
+				alert("You Lose");
+				
+			};
+
+			if (defenderCharacterStats.hp < 1) {
+				opponentsLeft = opponentsLeft - 1;
+
+				resetDefender();
+
+				$('#attack').remove();
+
+				// Selecting Warrior as Defender (2nd Time)
+				if ($(this).is($('#warrior')) && ($(this).hasClass('opponent'))) {
+					defenderRow = $('<div id="defenderRow" class="row"></div>');
+					defender = $('#warrior');
+					defenderCharacterStats = warrior;
+					opponentsRow.after(defenderRow);
+					defenderRow.append(defender);
+					attack = $('<button id="attack">Attack!</button>');
+					$('#userRow').append(attack);
+					$('#warrior').unbind();
+
+					displayDefenderStats = $('<div id="displayDefenderStats" class="col-xs-2">').html('Health: ' + defenderCharacterStats.hp + '<br>Attack: ' + defenderCharacterStats.ap + '<br>Counter: ' + defenderCharacterStats.ca);
+					defenderRow.append(displayDefenderStats);
+				};
+
+				// Selecting Girl as Defender (2nd Time)
+				if ($(this).is($('#girl')) && ($(this).hasClass('opponent'))) {
+					defenderRow = $('<div id="defenderRow" class="row"></div>');
+					defender = $('#girl');
+					defenderCharacterStats = girl;
+					opponentsRow.after(defenderRow);
+					defenderRow.append(defender);
+					attack = $('<button id="attack">Attack!</button>');
+					$('#userRow').append(attack);
+					$('#girl').unbind();
+
+					displayDefenderStats = $('<div id="displayDefenderStats" class="col-xs-2">').html('Health: ' + defenderCharacterStats.hp + '<br>Attack: ' + defenderCharacterStats.ap + '<br>Counter: ' + defenderCharacterStats.ca);
+					defenderRow.append(displayDefenderStats);
+				};
+
+				// Selecting Goblin as Defender (2nd Time)
+				if ($(this).is($('#goblin')) && ($(this).hasClass('opponent'))) {
+					defenderRow = $('<div id="defenderRow" class="row"></div>');
+					defender = $('#goblin');
+					defenderCharacterStats = goblin;
+					opponentsRow.after(defenderRow);
+					defenderRow.append(defender);
+					attack = $('<button id="attack">Attack!</button>');
+					$('#userRow').append(attack);
+					$('#goblin').unbind();
+
+					displayDefenderStats = $('<div id="displayDefenderStats" class="col-xs-2">').html('Health: ' + defenderCharacterStats.hp + '<br>Attack: ' + defenderCharacterStats.ap + '<br>Counter: ' + defenderCharacterStats.ca);
+					defenderRow.append(displayDefenderStats);
+				};
+ 
+				// Selecting Giant as Defender (2nd Time)
+				if ($(this).is($('#giant')) && ($(this).hasClass('opponent'))) {
+					defenderRow = $('<div id="defenderRow" class="row"></div>');
+					defender = $('#giant');
+					defenderCharacterStats = giant;
+					opponentsRow.after(defenderRow);
+					defenderRow.append(defender);
+					attack = $('<button id="attack">Attack!</button>');
+					$('#userRow').append(attack);
+					$('#giant').unbind();
+
+					displayDefenderStats = $('<div id="displayDefenderStats" class="col-xs-2">').html('Health: ' + defenderCharacterStats.hp + '<br>Attack: ' + defenderCharacterStats.ap + '<br>Counter: ' + defenderCharacterStats.ca);
+					defenderRow.append(displayDefenderStats);
+				};
+
+				
+
+
+			}
+
+			if (opponentsLeft < 1) {
+					
+					console.log("start a new game");
+
+					$('#userRow').append('<div id="warrior" class="col-xs-2 character"><a href="#"><img class="media-object" src="https://github.com/hcbreedl/week-4-game/blob/master/assets/images/warrior.png?raw=true" alt=""></a></div>');
+					$('#userRow').append('<div id="girl" class="col-xs-2 character"><a href="#"><img class="media-object" src="https://github.com/hcbreedl/week-4-game/blob/master/assets/images/girl.png?raw=true" alt=""></a></div>');
+					$('#userRow').append('<div id="goblin" class="col-xs-2 character"><a href="#"><img class="media-object" src="https://github.com/hcbreedl/week-4-game/blob/master/assets/images/goblin.png?raw=true" alt=""></a></div>');
+					$('#userRow').append('<div id="giant" class="col-xs-2 character"><a href="#"><img class="media-object" src="https://github.com/hcbreedl/week-4-game/blob/master/assets/images/giant.png?raw=true" alt=""></a></div> ');
+
+					// usersCharacter.remove();
+					// usersCharacterStats;
+					// displayUserStats.remove();
+					resetUserCharacter();
+
+					newGame();
+				}
 		});
+
+
+
+
 		
 			
 		
 	}); //End of on.click function
+};
+newGame();
 
 }); //End of Document.ready function
